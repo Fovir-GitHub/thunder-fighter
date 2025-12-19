656d54755e1d167848566b48a93aea1ce508b9c9
dazhuqi <1260930860@qq.com>
2025-12-19 22:48:53 +0800

    feat: Implement NormalEnemy class extending AbstractEnemyAircraft

--- src/main/java/org/thunderfighter/game/aircraft/enemy/NormalEnemy.java
+++ src/main/java/org/thunderfighter/game/aircraft/enemy/NormalEnemy.java
@@ -1,1 +1,27 @@
+package org.thunderfighter.game.aircraft.enemy;
 
+import javafx.geometry.Dimension2D;
+import org.thunderfighter.core.abstractor.AbstractEnemyAircraft;
+
+public class NormalEnemy extends AbstractEnemyAircraft {
+
+  public NormalEnemy(double x, double y) {
+    this.x = x;
+    this.y = y;
+    this.hp = 1;
+    this.speed = 2;
+    this.score = 10;
+    this.canShoot = false;
+    this.size = new Dimension2D(50, 60);
+  }
+
+  @Override
+  protected void move() {
+    y += speed;
+  }
+
+  @Override
+  protected void doShoot() {
+    // Normal enemy doesn't shoot
+  }
+}
