fcb2eb0e868307c3159bb3ca8b8eb1203050fd38
dazhuqi <1260930860@qq.com>
2025-12-19 22:49:33 +0800

    feat: Implement EliteEnemy class with movement and shooting

--- src/main/java/org/thunderfighter/game/aircraft/enemy/EliteEnemy.java
+++ src/main/java/org/thunderfighter/game/aircraft/enemy/EliteEnemy.java
@@ -1,1 +1,28 @@
+package org.thunderfighter.game.aircraft.enemy;
 
+import javafx.geometry.Dimension2D;
+import org.thunderfighter.core.abstractor.AbstractEnemyAircraft;
+
+public class EliteEnemy extends AbstractEnemyAircraft {
+
+  public EliteEnemy(double x, double y) {
+    this.x = x;
+    this.y = y;
+    this.hp = 3;
+    this.speed = 2;
+    this.score = 50;
+    this.canShoot = true;
+    this.shootInterval = 60;
+    this.size = new Dimension2D(60, 80);
+  }
+
+  @Override
+  protected void move() {
+    y += speed;
+  }
+
+  @Override
+  protected void doShoot() {
+    System.out.println("Elite enemy shoot");
+  }
+}
