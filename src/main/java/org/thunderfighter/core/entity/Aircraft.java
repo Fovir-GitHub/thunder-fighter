c97995b902dfefd4e4cb36530b3c319e817972a6
dazhuqi <1260930860@qq.com>
2025-12-19 22:51:00 +0800

    feat: clarify method comments in Aircraft interface
    Added comments to clarify method purposes in the Aircraft interface.

--- src/main/java/org/thunderfighter/core/entity/Aircraft.java
+++ src/main/java/org/thunderfighter/core/entity/Aircraft.java
@@ -3,13 +3,13 @@ package org.thunderfighter.core.entity;
 import javafx.geometry.Bounds;
 
 public interface Aircraft extends Entity {
-  int getHp();
+  int getHp(); // current health
 
   void takeDamage(int damage);
 
-  boolean isPlayer();
+  boolean isPlayer(); // is player aircraft or not
 
-  void shoot(); // new bullet here
+  void shoot(); // fire bullets, but only declares the "behavior", does not create the bullets
 
-  Bounds getCollisionBounds();
+  Bounds getCollisionBounds(); // obtain the boundary required for collision detection
 }
