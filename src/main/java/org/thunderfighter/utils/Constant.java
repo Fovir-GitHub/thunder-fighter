eaf9391f8e150ff06c5c9497a6538dc5e5f9d9a9
Fovir <fovir@disroot.org>
2025-12-24 10:06:12 +0800

    feat: add `Constant.java` to store global constants

---
+++ src/main/java/org/thunderfighter/utils/Constant.java
@@ -0,0 +1,19 @@
+package org.thunderfighter.utils;
+
+/** Constant */
+public class Constant {
+
+  public static final int ENEMY_NUMBER_LIMIT = 16;
+
+  public static final int SCORE_PER_ROUND = 1000;
+  public static final int GENERATE_ELITE_SCORE = 500;
+  public static final int GENERATE_BOSS_SCORE = 800;
+
+  public static final int TPS = 60;
+
+  public static enum Phase {
+    NORMAL,
+    ELITE,
+    BOSS
+  }
+}
