3c3cc0bc4aa26dd4cf3d50f03c2cf98db24175e9
Fovir <fovir@disroot.org>
2025-12-27 01:29:31 +0800

    style: format code

--- src/main/java/org/thunderfighter/ui/UiMenu.java
+++ src/main/java/org/thunderfighter/ui/UiMenu.java
@@ -5,35 +5,37 @@ import javafx.scene.text.Text;
 import org.thunderfighter.core.abstractor.AbstractUiMenu;
 
 public class UiMenu extends AbstractUiMenu {
-    public Button startButton = new Button("Start Game");
-    public Button historyButton = new Button("History Score");
-    public Button aboutButton = new Button("About");
+  public Button startButton = new Button("Start Game");
+  public Button historyButton = new Button("History Score");
+  public Button aboutButton = new Button("About");
 
-    private final UiOverlay overlay;
+  private final UiOverlay overlay;
 
-    public UiMenu(UiOverlay overlay) {
-        this.overlay = overlay;
+  public UiMenu(UiOverlay overlay) {
+    this.overlay = overlay;
 
-        Text title = new Text("Thunder Fighter");
-        this.setSpacing(20);
-        this.getChildren().addAll(title, startButton, historyButton, aboutButton);
-        this.setVisible(true);
+    Text title = new Text("Thunder Fighter");
+    this.setSpacing(20);
+    this.getChildren().addAll(title, startButton, historyButton, aboutButton);
+    this.setVisible(true);
 
-        startButton.setOnAction(e -> {
-            this.hideMenu();
-            overlay.showMenu();
+    startButton.setOnAction(
+        e -> {
+          this.hideMenu();
+          overlay.showMenu();
         });
 
-        historyButton.setOnAction(e -> UiScoreStorage.showScoreDialog());
-        aboutButton.setOnAction(e -> UiDialog.showAboutDialog());
-    }
-    @Override
-    public void showMenu() {
-        this.setVisible(true);
-    }
+    historyButton.setOnAction(e -> UiScoreStorage.showScoreDialog());
+    aboutButton.setOnAction(e -> UiDialog.showAboutDialog());
+  }
 
-    @Override
-    public void hideMenu() {
-        this.setVisible(false);
-    }
+  @Override
+  public void showMenu() {
+    this.setVisible(true);
+  }
+
+  @Override
+  public void hideMenu() {
+    this.setVisible(false);
+  }
 }
