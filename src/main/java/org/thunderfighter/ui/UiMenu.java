8fa98d5a2b778c6de37c7344f9c31232b6d380fb
Fovir <fovir@disroot.org>
2025-12-27 01:31:43 +0800

    refactor: remove member variable `overlay` and pass a `Game` instance to switch the game state

--- src/main/java/org/thunderfighter/ui/UiMenu.java
+++ src/main/java/org/thunderfighter/ui/UiMenu.java
@@ -3,17 +3,15 @@ package org.thunderfighter.ui;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 import org.thunderfighter.core.abstractor.AbstractUiMenu;
+import org.thunderfighter.game.Game;
+import org.thunderfighter.utils.Constant.GAME_STATE;
 
 public class UiMenu extends AbstractUiMenu {
   public Button startButton = new Button("Start Game");
   public Button historyButton = new Button("History Score");
   public Button aboutButton = new Button("About");
 
-  private final UiOverlay overlay;
-
-  public UiMenu(UiOverlay overlay) {
-    this.overlay = overlay;
-
+  public UiMenu(final Game game) {
     Text title = new Text("Thunder Fighter");
     this.setSpacing(20);
     this.getChildren().addAll(title, startButton, historyButton, aboutButton);
@@ -22,7 +20,7 @@ public class UiMenu extends AbstractUiMenu {
     startButton.setOnAction(
         e -> {
           this.hideMenu();
-          overlay.showMenu();
+          game.setGameState(GAME_STATE.RUNNING);
         });
 
     historyButton.setOnAction(e -> UiScoreStorage.showScoreDialog());
