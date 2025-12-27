3c03e74770893a805426e51146a69b2fcd0e7610
Fovir <fovir@disroot.org>
2025-12-27 21:01:58 +0800

    chore: reduce the socre of generating elite and boss enemy

--- src/main/java/org/thunderfighter/utils/Constant.java
+++ src/main/java/org/thunderfighter/utils/Constant.java
@@ -5,9 +5,8 @@ public class Constant {
 
   public static final int ENEMY_NUMBER_LIMIT = 16;
 
-  public static final int SCORE_PER_ROUND = 1000;
-  public static final int GENERATE_ELITE_SCORE = 500;
-  public static final int GENERATE_BOSS_SCORE = 800;
+  public static final int GENERATE_ELITE_SCORE = 10;
+  public static final int GENERATE_BOSS_SCORE = 100;
 
   public static final int TPS = 60;
 
