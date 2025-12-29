80c8b8244b9c915534e0e66fff23f2a3fc780474
dazhuqi <1260930860@qq.com>
2025-12-29 13:33:33 +0800

    docs: add comments to EliteEnemy class

--- src/main/java/org/thunderfighter/game/aircraft/enemy/EliteEnemy.java
+++ src/main/java/org/thunderfighter/game/aircraft/enemy/EliteEnemy.java
@@ -7,10 +7,24 @@ import org.thunderfighter.core.abstractor.AbstractEnemyAircraft;
 import org.thunderfighter.core.abstractor.AbstractEntity;
 import org.thunderfighter.game.bullet.NormalEnemyBullet;
 
+/**
+ * EliteEnemy
+ * Elite Enemy Plane
+ * Characteristics:
+ * - Low Health
+ * - Moves vertically downwards in a straight line
+ * - Periodically fires single, linear bullets
+ */
 public class EliteEnemy extends AbstractEnemyAircraft {
 
+  /** Elite enemy aircraft dimensions (width 60, height 80) */
   public static final Dimension2D SIZE = new Dimension2D(60, 80); // @params
 
+  /**
+   * Constructor
+   *
+   * @param x The X coordinate at birth (Y coordinate is fixed at the top of the screen)
+   */
   public EliteEnemy(double x) {
     this.size = SIZE;
 
@@ -26,16 +40,28 @@ public class EliteEnemy extends AbstractEnemyAircraft {
     this.sprite = new Image(getClass().getResourceAsStream("/images/Aircraft/EliteEnemy.png"));
   }
 
+  /**
+   * Update logic per frame
+   * Currently only calling parent class logic
+   */
   @Override
   protected void onUpdate(List<AbstractEntity> worldEntities) {
     super.onUpdate(worldEntities);
   }
 
+  /**
+   * Movement logic
+   * Elite enemy aircraft only move vertically downwards
+   */
   @Override
   protected void move() {
     y += speed;
   }
 
+  /**
+   * Shooting logic
+   * Fire a standard enemy bullet directly downwards
+   */
   @Override
   protected void doShoot(List<AbstractEntity> worldEntities) {
     NormalEnemyBullet bullet =
