46e002f5652d43c38e55f0909ebe7e4dcbc0625c
zhoulukai329-spec <zhoulukai329@gmail.com>
2025-12-29 12:19:08 +0800

    feat: add comments to ui dialog
    Added documentation comments to the UiDialog class to explain its purpose and usage.

--- src/main/java/org/thunderfighter/ui/UiDialog.java
+++ src/main/java/org/thunderfighter/ui/UiDialog.java
@@ -4,6 +4,13 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import org.thunderfighter.core.abstractor.AbstractUiDialog;
 
+/*
+1. UiDialog class for displaying various dialogs in the Thunder Fighter game.
+2. This class will be used in UiScoreStorage, UiMenu and etc. to show dialogs.
+3. In this class, we define methods to show information dialogs, about dialog, and game rule dialog.
+4. Therefore, we can reuse this class to show different types of dialogs 
+   by using the methods showInfoDialog, showAboutDialog, and showRuleDialog.
+*/
 public class UiDialog extends AbstractUiDialog {
   private final Alert alert;
 
@@ -26,11 +33,11 @@ public class UiDialog extends AbstractUiDialog {
     alert.setHeaderText(null);
     alert.setContentText(message);
     alert.showAndWait();
-  } // show generic information dialog
+  } // show generic information of dialog
 
   public static void showAboutDialog() {
     showInfoDialog("About", "Thunder Fighter © 2025\nAll rights reserved.");
-  }
+  }// show the copyright in dialog
 
   public static void showRuleDialog() {
     showInfoDialog(
@@ -39,5 +46,6 @@ public class UiDialog extends AbstractUiDialog {
             + "2. Press space to shoot.\n"
             + "3. Avoid enemy fire and obstacles.\n"
             + "4. Score points by destroying enemies.\n");
-  } // show game rule dialog
+  } // show game rule dialog in dialog
 }
+
