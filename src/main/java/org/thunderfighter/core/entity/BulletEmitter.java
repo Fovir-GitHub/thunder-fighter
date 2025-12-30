684d00c66d9921397dcef6e2945e32673e4fc9ab
dazhuqi <1260930860@qq.com>
2025-12-30 09:11:05 +0800

    docs: complete entity.yaml

--- src/main/java/org/thunderfighter/core/entity/BulletEmitter.java
+++
@@ -1,9 +0,0 @@
-// src/main/java/org/thunderfighter/core/entity/BulletEmitter.java
-
-package org.thunderfighter.core.entity;
-
-public interface BulletEmitter {
-  void emit(Bullet bullet);
-}
-// A simple callback interface to decouple Aircraft.shoot() from the world bullet list.
-// Aircraft can call emitter.emit(...) without directly accessing collections in the game world.
